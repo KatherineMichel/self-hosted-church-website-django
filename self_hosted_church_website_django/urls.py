128393fee61d2a9c2e7481b2f34269205c0a945a
Katherine Michel <kthrnmichel@gmail.com>
2016-12-23 22:01:00 -0600

    Fixing photos

--- self_hosted_church_website_django/urls.py
+++ self_hosted_church_website_django/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^activities/$', TemplateView.as_view(template_name="activities.html")),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
     url(r'^admin/', admin.site.urls),
 ]
