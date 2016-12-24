375080f3f7f039862da9d54b0764fc2903798922
Katherine Michel <kthrnmichel@gmail.com>
2016-12-24 00:17:58 -0600

    Conforming URLs to Django standards

--- self_hosted_church_website_django/urls.py
+++ self_hosted_church_website_django/urls.py
@@ -3,18 +3,19 @@ from django.views.generic.base import TemplateView
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^$', TemplateView.as_view(template_name="home.html")),
-    url(r'^activities/$', TemplateView.as_view(template_name="activities.html")),
-    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
-    url(r'^children-and-youth-ministries/$', TemplateView.as_view(template_name="detail-pages/children-and-youth-ministries.html")),
-    url(r'^church/$', TemplateView.as_view(template_name="detail-pages/church.html")),
-    url(r'^community-outreach/$', TemplateView.as_view(template_name="detail-pages/community-outreach.html")),
-    url(r'^global-church/$', TemplateView.as_view(template_name="detail-pages/global-church.html")),
-    url(r'^music-ministry/$', TemplateView.as_view(template_name="detail-pages/music-ministry.html")),
-    url(r'^pretty-prairie/$', TemplateView.as_view(template_name="detail-pages/pretty-prairie.html")),
-    url(r'^united-methodist-women/$', TemplateView.as_view(template_name="detail-pages/united-methodist-women.html")),
-    url(r'^welcome/$', TemplateView.as_view(template_name="detail-pages/welcome.html")),
-    url(r'^worship/$', TemplateView.as_view(template_name="detail-pages/worship.html")),
+	url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
+    url(r'^activities/$', TemplateView.as_view(template_name="activities.html"), name="activities"),
+    url(r'^blog/$', TemplateView.as_view(template_name="blog.html"), name="blog"),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^children-and-youth-ministries/$', TemplateView.as_view(template_name="detail-pages/children-and-youth-ministries.html"), name="children-and-youth-ministries"),
+    url(r'^church/$', TemplateView.as_view(template_name="detail-pages/church.html"), name="church"),
+    url(r'^community-outreach/$', TemplateView.as_view(template_name="detail-pages/community-outreach.html"), name="community-outreach"),
+    url(r'^global-church/$', TemplateView.as_view(template_name="detail-pages/global-church.html"), name="global-church"),
+    url(r'^music-ministry/$', TemplateView.as_view(template_name="detail-pages/music-ministry.html"), name="music-ministry"),
+    url(r'^pretty-prairie/$', TemplateView.as_view(template_name="detail-pages/pretty-prairie.html"), name="pretty-prairie"),
+    url(r'^united-methodist-women/$', TemplateView.as_view(template_name="detail-pages/united-methodist-women.html"), name="united-methodist-women"),
+    url(r'^welcome/$', TemplateView.as_view(template_name="detail-pages/welcome.html"), name="welcome"),
+    url(r'^worship/$', TemplateView.as_view(template_name="detail-pages/worship.html"), name="worship"),
     url(r'^admin/', admin.site.urls),
 ]
 
