fce762a079d9c3408436e7180618f031bde1cf40
Katherine Michel <kthrnmichel@gmail.com>
2016-12-24 20:48:09 -0600

    Adding post detail pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.blog, name='blog'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post-detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def blog(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog.html', {'posts': posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post-detail.html', {'post': post})
+
