c33dc55b9d5a043c3d6b0a2633e7e6ffa2524c04
Katherine Michel <kthrnmichel@gmail.com>
2016-12-25 22:54:57 -0600

    Making a few improvements to style

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
